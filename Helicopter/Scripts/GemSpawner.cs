b0919ca2751b3db663c53ca6f6f0eae2d0d5dfa7
dhruvbhatt <63405689+dhruvbhatt@users.noreply.github.com>
2021-10-10 19:24:04 +0530

    Gems Spawn and Scroll Reset update.
    Background speed increase as the game progresses an on game over, it resets. It also randomly spawns gem which is worth 10 points.

---
+++ Helicopter/Scripts/GemSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawner : MonoBehaviour
+{
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		// infinite coin spawning function, asynchronous
+		StartCoroutine(SpawnGems());
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	IEnumerator SpawnGems()
+	{
+		while (true)
+		{
+
+			// number of coins we could spawn vertically
+			int gemsRender = Random.Range(1, 3);
+			if(gemsRender==1)
+			{
+			// instantiate all coins in this row separated by some random amount of space
+				Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+			
+			}
+			// pause 1-5 seconds until the next coin spawns
+			yield return new WaitForSeconds(Random.Range(1, 5));
+		}
+	}
+}
